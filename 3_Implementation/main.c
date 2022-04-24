73aefff2649ebf161617f397d45d9ed2d9f6462c
arnoorlasravan <101430667+arnoorlasravan@users.noreply.github.com>
2022-04-24 14:16:27 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -10,17 +10,16 @@
 int main(void)
 
 {
-	DDRB=DDRB & 0b11111101;//fd Input pin b of controller logic 0 for high
+	DDRB=DDRB & 0b11111101;//fd FOR HIGH
 
-	DDRC=DDRC | 0b01000000;//40 Output pin c of controller logic 1 for low
-
+	DDRC=DDRC | 0b01000000;//40 FOR LOW
 	while(1)
 
 	{
         if(PINB & 0b00000010)//02 check door open 
-			PORTC=PORTC | 0b01000000;//40 seted high LED
+			PORTC=PORTC | 0b01000000;//40  SETED HIGH LED
 		else
-			PORTC=PORTC & 0b10111111;//bf seted low LED
+			PORTC=PORTC & 0b10111111;//bf SETED LOW LED
 	}
 
 	return 0;
